20bb39506c57211f2a48581b9e762ea2d56804e8
Neyton <193637348+neytonnordino@users.noreply.github.com>
2025-07-02 11:14:35 +0200

    commit

---
+++ claudio/app/components/ChatMessage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const chatMessage = () => {
+  return (
+    <div>chatMessage</div>
+  )
+}
+
+export default chatMessage
---
+++ claudio/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <h1 className="text-4xl">Hello, i'm Claudio, your AI assistant!</h1>;
+}
+
+
